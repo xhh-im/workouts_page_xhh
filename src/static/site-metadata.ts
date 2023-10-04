d510f1d7ad70ddf8a8a6b64b4492e78b5c3fde08
arthurfsy2 <129527972+arthurfsy2@users.noreply.github.com>
2023-10-05 00:53:52 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,7 +11,7 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Workouts Map',
+  siteTitle: 'Feng Siyuan锻炼记录',
   siteUrl: 'https://blog.4a1801.life',
   logo: 'https://pan.4a1801.life/d/Onedrive-4A1801/%E4%B8%AA%E4%BA%BA%E5%BB%BA%E7%AB%99/public/logo1.jpg',
   description: 'Personal site and blog',
