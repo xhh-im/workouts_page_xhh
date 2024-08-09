f8d00f64754d3dcad7e9c891bbd0b77674075d8b
arthurfsy2 <fsyflh@gmail.com>
2024-08-09 17:34:22 +0800

    changed

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,18 +12,18 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Feng Siyuan锻炼记录',
-  siteUrl: 'https://blog.4a1801.life',
-  logo: 'https://pan.4a1801.life/d/Onedrive-4A1801/%E4%B8%AA%E4%BA%BA%E5%BB%BA%E7%AB%99/public/logo1.jpg',
+  siteUrl: 'https://fengsy.cn',
+  logo: 'https://pan.4a1801.life:11443/d/NAS/logo1.jpg',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://blog.4a1801.life',
+      name: '家庭博客',
+      url: 'https://fengsy.cn',
     },
     {
-      name: 'About',
-      url: 'https://blog.4a1801.life/%E5%AE%B6%E5%BA%AD/intro.html',
+      name: '关于我们',
+      url: 'https://fengsy.cn/%E5%AE%B6%E5%BA%AD/intro.html',
     },
   ],
 };
