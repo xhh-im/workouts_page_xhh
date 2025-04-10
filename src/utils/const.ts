b001b4a135d6fecb7a59c1ec9c7eb4e3b14bb795
arthurfsy2 <129527972+arthurfsy2@users.noreply.github.com>
2025-04-10 10:55:09 +0800

    test

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -5,10 +5,10 @@ const MAPBOX_TOKEN =
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
   'pk.eyJ1IjoiYmVuLTI5IiwiYSI6ImNrZ3Q4Ym9mMDBqMGYyeXFvODV2dWl6YzQifQ.gSKoWF-fMjhzU67TuDezJQ';
 const MUNICIPALITY_CITIES_ARR = [
-  // '北京市',
-  // '上海市',
-  // '天津市',
-  // '重庆市',
+  '北京市',
+  '上海市',
+  '天津市',
+  '重庆市',
   '香港特别行政区',
   '澳门特别行政区',
 ];
